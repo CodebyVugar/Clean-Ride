40109f65eb7be3f600a3e4f2a616a7c3eb3daca8
CodebyVugar <ahmedovvugar005@gmail.com>
2024-11-23 19:58:48 +0400

    Forget pass site

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,27 +1,34 @@
 const forms = document.querySelector(".forms"),
-      pwShowHide = document.querySelectorAll(".eye-icon"),
-      links = document.querySelectorAll(".link");
+    pwShowHide = document.querySelectorAll(".eye-icon"),
+    links = document.querySelectorAll(".link");
 
 pwShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-        
-        pwFields.forEach(password => {
-            if(password.type === "password"){
-                password.type = "text";
-                eyeIcon.classList.replace("bx-hide", "bx-show");
-                return;
-            }
-            password.type = "password";
-            eyeIcon.classList.replace("bx-show", "bx-hide");
-        })
-        
-    })
-})      
+        let password = eyeIcon.previousElementSibling;
+
+        if (password.type === "password") {
+            password.type = "text";
+            eyeIcon.classList.replace("bx-hide", "bx-show");
+            return;
+        }
+        password.type = "password";
+        eyeIcon.classList.replace("bx-show", "bx-hide");
+    });
+});
 
 links.forEach(link => {
     link.addEventListener("click", e => {
-       e.preventDefault(); //preventing form submit
-       forms.classList.toggle("show-signup");
-    })
-})
+        e.preventDefault(); // Prevent default form submission behavior
+
+        if (link.classList.contains('signup-link')) {
+            forms.classList.add("show-signup");
+            forms.classList.remove("show-forget-password");
+        } else if (link.classList.contains('login-link')) {
+            forms.classList.remove("show-signup");
+            forms.classList.remove("show-forget-password");
+        } else if (link.classList.contains('forget-password-link')) {
+            forms.classList.add("show-forget-password");
+            forms.classList.remove("show-signup");
+        }
+    });
+});
